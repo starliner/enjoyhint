68ae9128de8165965032cf031cb52c9d78f1a9e5
d.shamin <d.shamin@starliner.ru>
2019-07-09 13:46:47 +0300

    refs #215101 Set default margin value to 0

--- dist/enjoyhint.js
+++ dist/enjoyhint.js
@@ -261,7 +261,7 @@ var EnjoyHint = function (_options) {
                     var max_habarites = Math.max(w, h);
                     var radius = step_data.radius || Math.round(max_habarites / 2) + 5;
                     var offset = $element.offset();
-                    var shape_margin = (step_data.margin !== undefined) ? step_data.margin : 10;
+                    var shape_margin = (step_data.margin !== undefined) ? step_data.margin : 0;
 
                     var coords = {
                         x: offset.left + Math.round(w / 2),
